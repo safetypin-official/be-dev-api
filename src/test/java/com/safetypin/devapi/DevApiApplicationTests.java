728b0bc13aa10c7bb38953b21f7e7a98cf2feeba
Muhammad Raihan Akbar <ianakbar711@gmail.com>
2025-02-14 20:02:56 +0700

    Initial commit

---
+++ src/test/java/com/safetypin/devapi/DevApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetypin.devapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
